3e01678f1b06ba44bd7eb7dc8a623c2c6b75939a
gabboraron <gabboraron@gmail.com>
2021-03-10 17:51:46 +0100

    bead-1

---
+++ kodelmelete_hf1.js
@@ -0,0 +1,49 @@
+"use strict"
+
+var mystring = "";
+var a = 0;
+var b = 0;
+var c = 0;
+var d = 0;
+var e = 0;
+
+for (var i = 0; i < 40; i++) {
+	chID = Math.floor(Math.random() * 5); 
+	switch(chID){
+		case 0: mystring += "a"; a++; break;
+		case 1: mystring += "b"; b++; break;
+		case 2: mystring += "c"; c++; break;
+		case 3: mystring += "d"; d++; break;
+		case 4: mystring += "e"; e++; break;
+	}
+}
+
+console.log("karaktersorozat: " + mystring);
+console.log("karakterek darabszáma: ");
+console.log("a: " + a);
+console.log("b: " + b);
+console.log("c: " + c);
+console.log("d: " + d);
+console.log("e: " + e);
+
+console.log(" ");
+
+console.log("Valószínűségi eloszlások");
+console.log("p(a): " + a/(a+b+c+d+e));
+console.log("p(b): " + b/(a+b+c+d+e));
+console.log("p(c): " + c/(a+b+c+d+e));
+console.log("p(d): " + d/(a+b+c+d+e));
+console.log("p(e): " + e/(a+b+c+d+e));
+
+console.log(" ");
+
+console.log("Információmennyiségek: ");
+console.log("I(a): " + Math.log2(1/(a/(a+b+c+d+e))));
+console.log("I(b): " + Math.log2(1/(b/(a+b+c+d+e))));
+console.log("I(c): " + Math.log2(1/(c/(a+b+c+d+e))));
+console.log("I(d): " + Math.log2(1/(d/(a+b+c+d+e))));
+console.log("I(e): " + Math.log2(1/(e/(a+b+c+d+e))));
+
+console.log(" ");
+
+console.log("H(x): " + (-1)*( Math.log2(1/(a/(a+b+c+d+e)))*a/(a+b+c+d+e) +  Math.log2(1/(b/(a+b+c+d+e)))*b/(a+b+c+d+e) + Math.log2(1/(c/(a+b+c+d+e)))*c/(a+b+c+d+e) + Math.log2(1/(d/(a+b+c+d+e)))* d/(a+b+c+d+e) + Math.log2(1/(e/(a+b+c+d+e)))*e/(a+b+c+d+e)) );
