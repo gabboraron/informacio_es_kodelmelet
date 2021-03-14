bc029be3b6280c66d9302a5d91fa637916327bd2
gabboraron <gabboraron@gmail.com>
2021-03-14 01:37:50 +0100

    bead-1

--- kodelmelete_hf1.js
+++ kodelmelete_hf1.js
@@ -8,7 +8,7 @@ var d = 0;
 var e = 0;
 
 for (var i = 0; i < 40; i++) {
-	chID = Math.floor(Math.random() * 5); 
+	var chID = Math.floor(Math.random() * 5); 
 	switch(chID){
 		case 0: mystring += "a"; a++; break;
 		case 1: mystring += "b"; b++; break;
@@ -47,3 +47,17 @@ console.log("I(e): " + Math.log2(1/(e/(a+b+c+d+e))));
 console.log(" ");
 
 console.log("H(x): " + (-1)*( Math.log2(1/(a/(a+b+c+d+e)))*a/(a+b+c+d+e) +  Math.log2(1/(b/(a+b+c+d+e)))*b/(a+b+c+d+e) + Math.log2(1/(c/(a+b+c+d+e)))*c/(a+b+c+d+e) + Math.log2(1/(d/(a+b+c+d+e)))* d/(a+b+c+d+e) + Math.log2(1/(e/(a+b+c+d+e)))*e/(a+b+c+d+e)) );
+
+var encodedString = "";
+for (var i = 0; i < mystring.length; i++) {
+	switch(mystring[i]){
+		case "a": encodedString += "0"; 	 break;
+		case "b": encodedString += "10";  	 break;
+		case "c": encodedString += "110";  	 break;
+		case "d": encodedString += "111";  	 break;
+		case "e": encodedString += "1110";   break;
+	}
+}
+
+console.log("encoded: " + encodedString);
+
